0577695f5a2c8b1db51271ff5b43a6a04adc4857
kasjaduddin <kasjaduddin@outlook.com>
2024-03-26 10:39:55 +0700

    feat: add drawing ellips and rectangle

---
+++ GraphicsTransformationApp/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace GraphicsTransformationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonScale = new System.Windows.Forms.Button();
+            this.buttonRectangle = new System.Windows.Forms.Button();
+            this.buttonTranslation = new System.Windows.Forms.Button();
+            this.buttonElips = new System.Windows.Forms.Button();
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.buttonScale);
+            this.panel1.Controls.Add(this.buttonRectangle);
+            this.panel1.Controls.Add(this.buttonTranslation);
+            this.panel1.Controls.Add(this.buttonElips);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(2028, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonScale
+            // 
+            this.buttonScale.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.buttonScale.Location = new System.Drawing.Point(1157, 12);
+            this.buttonScale.Name = "buttonScale";
+            this.buttonScale.Size = new System.Drawing.Size(109, 77);
+            this.buttonScale.TabIndex = 3;
+            this.buttonScale.Text = "Scale";
+            this.buttonScale.UseVisualStyleBackColor = false;
+            // 
+            // buttonRectangle
+            // 
+            this.buttonRectangle.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.buttonRectangle.Location = new System.Drawing.Point(775, 12);
+            this.buttonRectangle.Name = "buttonRectangle";
+            this.buttonRectangle.Size = new System.Drawing.Size(109, 77);
+            this.buttonRectangle.TabIndex = 2;
+            this.buttonRectangle.Text = "Rectangle";
+            this.buttonRectangle.UseVisualStyleBackColor = false;
+            this.buttonRectangle.Click += new System.EventHandler(this.buttonRectangle_Click);
+            // 
+            // buttonTranslation
+            // 
+            this.buttonTranslation.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.buttonTranslation.Location = new System.Drawing.Point(960, 12);
+            this.buttonTranslation.Name = "buttonTranslation";
+            this.buttonTranslation.Size = new System.Drawing.Size(109, 77);
+            this.buttonTranslation.TabIndex = 1;
+            this.buttonTranslation.Text = "Translation";
+            this.buttonTranslation.UseVisualStyleBackColor = false;
+            // 
+            // buttonElips
+            // 
+            this.buttonElips.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.buttonElips.Location = new System.Drawing.Point(579, 12);
+            this.buttonElips.Name = "buttonElips";
+            this.buttonElips.Size = new System.Drawing.Size(109, 77);
+            this.buttonElips.TabIndex = 0;
+            this.buttonElips.Text = "Elips";
+            this.buttonElips.UseVisualStyleBackColor = false;
+            this.buttonElips.Click += new System.EventHandler(this.buttonElips_Click);
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.SystemColors.Window;
+            this.Canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Canvas.Location = new System.Drawing.Point(0, 0);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(2028, 744);
+            this.Canvas.TabIndex = 1;
+            this.Canvas.TabStop = false;
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            this.Canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseMove);
+            this.Canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label18);
+            this.panel2.Controls.Add(this.label17);
+            this.panel2.Controls.Add(this.label16);
+            this.panel2.Controls.Add(this.label15);
+            this.panel2.Controls.Add(this.label14);
+            this.panel2.Controls.Add(this.label13);
+            this.panel2.Controls.Add(this.label12);
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(1660, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(368, 644);
+            this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Graphics Transformation App";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(243, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "1. Aplikasi ini hanya dapat ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(48, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(309, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "digambar dengan menekan button";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(283, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "2. Pilih bangun datar yang ingin";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(48, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(289, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "menggambar satu bangun datar";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(30, 446);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(321, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "6. Pilih transformasi yang diinginkan";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(30, 499);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(316, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "7. Masukkan data yang dibutuhkan";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(25, 371);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(296, 25);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "5. Lepas tombol kiri mouse untuk";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(48, 524);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(261, 25);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "untuk melakukan tranformasi";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(25, 232);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(298, 25);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "3. Tekan tombol kiri mouse untuk";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(48, 183);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(230, 25);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "pada bagian atas canvas";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(48, 257);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(178, 25);
+            this.label15.TabIndex = 14;
+            this.label15.Text = "mulai menggambar";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(25, 303);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(235, 25);
+            this.label16.TabIndex = 15;
+            this.label16.Text = "4. Gerakkan mouse untuk";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(48, 328);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(317, 25);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "memperbesar ukuran bangun datar";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(48, 396);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(247, 25);
+            this.label18.TabIndex = 17;
+            this.label18.Text = "menggambar bangun datar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2028, 744);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Canvas);
+            this.Name = "Form1";
+            this.Text = "Graphics Transformation App by Kholil Asjaduddin";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonScale;
+        private System.Windows.Forms.Button buttonRectangle;
+        private System.Windows.Forms.Button buttonTranslation;
+        private System.Windows.Forms.Button buttonElips;
+        private System.Windows.Forms.PictureBox Canvas;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+    }
+}
+
---
+++ GraphicsTransformationApp/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsTransformationApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            bm = new Bitmap(Canvas.Width, Canvas.Height);
+            g = Graphics.FromImage(bm);
+            g.Clear(Color.White);
+            Canvas.Image = bm;
+        }
+
+        Bitmap bm;
+        Graphics g;
+        bool paint = false;
+        Pen pen = new Pen(Color.DarkBlue, 1);
+        int index;
+        int x, y, sX, sY, cX, cY;
+        bool canDraw = true;
+        int sX1, sY1, cX1, cY1;
+
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+            cX = e.X; 
+            cY = e.Y;
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            Canvas.Refresh();
+
+            x = e.X;
+            y = e.Y;
+            sX = e.X - cX;
+            sY = e.Y - cY;
+        }
+
+        private void Canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+            sX = x - cX;
+            sY = y - cY;
+
+            if (canDraw)
+            {
+                if (index == 1)
+                {
+                    g.DrawEllipse(pen, cX, cY, sX, sY);
+                    canDraw = false;
+                }
+                else if (index == 2)
+                {
+                    g.DrawRectangle(pen, cX, cY, sX, sY);
+                    canDraw = false;
+                }
+                cX1 = cX; 
+                cY1 = cY;
+                sX1 = sX;
+                sY1 = sY;
+            }
+            
+        }
+
+        private void buttonElips_Click(object sender, EventArgs e)
+        {
+            index = 1;
+        }
+
+        private void buttonRectangle_Click(object sender, EventArgs e)
+        {
+            index = 2;
+        }
+    }
+}
